d1b833978a3c7e39552bc043eacc459c85be2f7f
sr <7518647+wendychaung@users.noreply.github.com>
2023-05-02 15:57:05 +0800

    update version

---
+++ version.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const PackageJson = require("./src/package.template.json");
+
+let verArr = PackageJson.version.split(".");
+verArr[2] = Number(verArr[2]) + 1;
+let version = verArr.join(".")
+PackageJson.version = version;
+
+const functionStr = `${JSON.stringify(PackageJson, null, 4)}`;
+
+fs.writeFile("./src/package.template.json", functionStr, (err, data) => {
+    if (err) throw err;
+    console.log("new version: " + version);
+
+    fs.copyFile("./src/package.template.json", "./dist/package.json", (err) => {
+        if (err) throw err;
+        console.log("copy 'src/package.template.json' to 'dist/package.json'")
+    })
+});
+
