e367ebc4e52ddef837a82d22a006636cfc0302b8
sr <7518647+wendychaung@users.noreply.github.com>
2023-10-08 01:09:59 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,19 +13,19 @@ function App() {
                 blockchain: 'ethereum',
                 amount:0.0001,
                 // token: '0xdac17f958d2ee523a2206206994597c13d831ec7',
-                receiver: '0x4e260bB2b25EC6F3A59B478fCDe5eD5B8D783B02'
+                receiver: '0xD85c413dA833CeBD8338138CcEFA04979DF70E8e'
             },
             {
                 blockchain: 'bscTest',
                 amount:1,
-                token: '0x387Bd586130a03e4c2ae3c9b9b5481C0993Bfb00',
-                receiver: '0x183F09C3cE99C02118c570e03808476b22d63191'
+                token: '0xdf43C48D8Cd91D33Fa46bA0E5030293A19998dC2',
+                receiver: '0xD85c413dA833CeBD8338138CcEFA04979DF70E8e'
             },
             {
                 blockchain: 'bscTest',
                 amount:1,
                 // token: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-                receiver: '0x183F09C3cE99C02118c570e03808476b22d63191'
+                receiver: '0xD85c413dA833CeBD8338138CcEFA04979DF70E8e'
             }
         ]
     } />
